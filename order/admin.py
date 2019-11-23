61e38a25d01377c98a55b9ed0fe1d131edb2f8fb
ssseungzz7@gmaal.com <kimseunghyeon@SeunghyeonBook.local>
2019-11-24 04:32:43 +0900

    테이블, 메뉴, 주문 정보, 주문 세부 내역 모델 생성 및 admin에 추가

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Table, Menu, OrderInfo, OrderDetail
 
-# Register your models here.
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderInfo)
+class OrderInfoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    pass
--- order/models.py
+++ order/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Table(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Menu(models.Model):
+    category = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class OrderInfo(models.Model):
+    table_info = models.ForeignKey(Table, on_delete=models.CASCADE)
+    customer_num = models.IntegerField()
+    payment = models.BooleanField()
+    # 총 금액, 알바생 정보 필요
+    price_sum = models.IntegerField()
+
+    def __str__(self):
+        return f'담당자: , 주문번호: {self.id}'
+
+    def get_bill(self):
+        price_sum = 0
+        order_detail_list = self.orderdetail_set.all()
+        for order in order_detail_list:
+            price_sum += order.menu_info.price * order.menu_count
+        return price_sum
+
+    def save(self):
+        # 총 금액 계산해서 저장하도록 한다
+        pass
+
+
+class OrderDetail(models.Model):
+    order_info = models.ForeignKey(OrderInfo, on_delete=models.CASCADE)
+    menu_info = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    menu_count = models.IntegerField()
+
+    def __str__(self):
+        return f'주문번호: {self.order_info}, 메뉴 정보: {self.menu_info}, 수량: {self.menu_count}'
